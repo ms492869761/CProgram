8d30fb42f88cc22a6542d59afde04d5cb622f5fa
王志远 <ms492869761@gmail.com>
2019-11-26 14:58:31 +0800

    first commit

---
+++ CProgram/CProgram/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CProgram
+//
+//  Created by 王志远 on 2019/11/26.
+//  Copyright © 2019 王志远. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
